1551f7fa6cca8a35f7e45c6aae7c30441b8e1236
suyanlong <5929238410@qq.com>
2016-12-01 15:47:41 +0800

    调用约定的学习，stdcall cdecl
    Signed-off-by: suyanlong <5929238410@qq.com>

--- StudyMakefile/sources/StudyMakefile/StudyMakefile.cpp
+++ StudyMakefile/sources/StudyMakefile/StudyMakefile.cpp
@@ -1,27 +1,34 @@
-// StudyMakefile.cpp : 定义控制台应用程序的入口点。
-//
+//#include <iostream>
+//#include <string>
+//using namespace std;
 
-
-#include <string>
-#include <iostream>
 #include <stdio.h>
-#include <stdlib.h>
-#include "list"
-#include "./Tool/Tool.h"
 
+/*!
+ *2016/12/01 10:16:57
+ *\author suyanlong
+ *	1、是否能用作函数指针，或者能否用做回调与调用约定没有直接关系，而与函数指针类型有关系。
+ *  2、因此stdcall  cdecl 等调用约定，都是可以用作回调、事件注册机制里面，只是说一定要符合函数类型声明。
+ *  3、stdcall标准调用约定， cdecl 
+ *  4、关于线程是一个函数，然而既然是一个函数，都有调用约定，windows API 基本都是_stdcall约定。其他函数库是默认cdecl约定，但是不会影响参数的出栈。
+ *  
+ */
 
-using namespace std;
-using namespace Tool;
+typedef int (_stdcall *FUN)(int a,int b);
 
-int main(int argc, char* argv[])
-{
-	
-	int ret = Tool::TAdd(2,4);
-	cerr<< "输出值2222222222222222222222222222222222222222222：" << ret <<endl; 
-	//flush(cerr);
-	printf("----%d\n",ret);
-	//system("pause");
-	getchar();
-	return 0;
+int  _stdcall add(int a,int b){	  
+   return a+b;	
 }
 
+typedef int (_cdecl *FUN1)(int a,int b);
+
+int  _cdecl add1(int a,int b){	  
+	return a+b;	
+}
+
+int main(){	 
+
+	FUN f = add;
+	int k = f(2,3);
+	return 0;
+}
